e3e2370e826721b6ad0297a408a899abcd396917
namDevnrtcore <hahoainam4321@gmail.com>
2026-07-04 12:19:59 +0700

    fixxx

--- backend/obj/Debug/net7.0/CarTechShowcase.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/CarTechShowcase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarTechShowcase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17f2ea2f72bff8eda9ab6163a038a62063adc4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a14bf94fc00622f7408fcc267a20de718245de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarTechShowcase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarTechShowcase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
